4cd69edbcc7998e75cf3dabf90142d7c85da5a2c
Gonçalo Almeida <52497560+goncaloalmeida20@users.noreply.github.com>
2022-10-26 10:27:45 +0100

    Update tree.c

--- Meta2/tree.c
+++ Meta2/tree.c
@@ -1,12 +1,20 @@
 Node* create_node(char * type, char * value) {
-
+    Node* node = (Node*) malloc(sizeof(Node));
+    if (node == NULL) exit(0); // No memory
+    node->type = type;
+	node->value = value;
+    node->bro = NULL;
+	node->son = NULL;
+    return node;
 }
 
 void add_son(Node * fat, Node * son) {
-
+    if (fat == NULL || son == NULL) return;
+    fat->son = son;
 }
 
-void print_tree(Node* n, int nivel){
+void print_tree(Node* n, int level){
+    if (n == NULL) return;
 
 }
 
