d88544d42254850046a98f2e15147d3adfbd735e
Gonçalo Almeida <52497560+goncaloalmeida20@users.noreply.github.com>
2022-10-26 10:50:36 +0100

    Update tree.h

--- Meta2/tree.h
+++ Meta2/tree.h
@@ -8,6 +8,7 @@ Node* root;
 
 Node* create_node(char * type, char * value);
 void add_son(Node * fat, Node * son);
+void add_bro(Node * n, Node * bro)
 
 void print_tree(Node* n, int level);
 void free_tree(Node* n);
