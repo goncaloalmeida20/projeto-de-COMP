c69edbb50e80fa314b03360273741b92f57f5ea4
Gonçalo Almeida <52497560+goncaloalmeida20@users.noreply.github.com>
2022-10-26 10:27:33 +0100

    Update tree.h

--- Meta2/tree.h
+++ Meta2/tree.h
@@ -2,7 +2,6 @@ typedef struct node{
 	char * type;
 	char * value;
 	struct node * son;
-    struct node * bro;
 } Node;
 
 Node* root;
@@ -10,5 +9,5 @@ Node* root;
 Node* create_node(char * type, char * value);
 void add_son(Node * fat, Node * son);
 
-void print_tree(Node* n, int nivel);
+void print_tree(Node* n, int level);
 void free_tree(Node* n);
