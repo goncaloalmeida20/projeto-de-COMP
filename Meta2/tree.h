763b3795a4d4c779db7ef2e70104803d96243364
joaobjsantos <93948049+joaobjsantos@users.noreply.github.com>
2022-11-02 15:03:31 +0000

    Update tree.h

--- Meta2/tree.h
+++ Meta2/tree.h
@@ -1,3 +1,9 @@
+/*
+	Autores:
+		Gonçalo Fernandes Diogo de Almeida, nº2020218868
+		João Bernardo de Jesus Santos, nº2020218995
+*/
+
 #ifndef TREE_H
 #define TREE_H
 
@@ -18,6 +24,8 @@ Node* add_bro(Node *n, Node *bro);
 
 Node* add_if(Node *expr, Node *statement_if, Node *statement_else);
 Node* add_while(Node *expr, Node *statement_while);
+
+Node* create_blocks(Node *statement1, Node* statement2);
 
 void print_tree(Node *n, int level);
 void free_tree(Node *n);
